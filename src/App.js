015d4858fb1149d295f61b1bfa700d51e16699df
weisermichael <weiserjmichael@gmail.com>
2022-05-23 18:24:04 -0700

    header component, begin display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+function Header() {
+  return(
+    <header>
+      <h1>Movie Listings</h1>
+      <h2>This website was built by Michael Weiser as a recruiting challenge</h2>
+    </header>
+  )
+}
+
+function MoviesDisplay() {
+
+  const [movieData, setMovieData] = useState([])
+  
+  return(
+    <div>
+
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
