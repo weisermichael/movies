0ce1b7519d1071414ef69d61c443091b0b160878
weisermichael <weiserjmichael@gmail.com>
2022-05-23 18:45:20 -0700

    add state for movies/search and effect for movie data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Header() {
   return(
@@ -13,16 +12,29 @@ function Header() {
 
 function MoviesDisplay() {
 
-  const [movieData, setMovieData] = useState([])
-  
   return(
-    <div>
+    <div class="movie-display">
 
     </div>
   )
 }
 
 function App() {
+  //init state for search text, movies array, and filtered movies array
+  const [search, setSearch] = useState('')
+  const [movieData, setMovieData] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+
+  //effect for movies database
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=666376e7ed7c6a4d1e103f6bfcfe0cbd&page=1')
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+            return(setMovieData(data.results))
+          })
+          .then(console.log(movieData))
+  }, [])
   return (
     <div className="App">
       <Header />
